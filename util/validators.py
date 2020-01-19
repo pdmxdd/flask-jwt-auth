5dedeb98afebaa506d2ba870b9c81528fda98355
paul <pdmxdd@gmail.com>
2020-01-19 16:34:42 -0600

    JSON missing/JSON incorrect

--- util/validators.py
+++ util/validators.py
@@ -6,13 +6,16 @@ from util.json_response import make_json_response
 def bad_json():
     return make_json_response({"status": "error", "body": "incorrect request JSON"}, 400)
 
+def missing_json():
+    return make_json_response({"status": "error", "body": "JSON missing"}, 400)
+
 
 def json_required(required_json_headers):
     def requirements(f):
         @wraps(f)
         def wrap(*args, **kwargs):
             if request.json is None:
-                return bad_json()
+                return missing_json()
             for header in required_json_headers:
                 if header not in request.json.keys():
                     return bad_json()
