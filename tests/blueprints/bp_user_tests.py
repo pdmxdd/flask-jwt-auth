4756eac3fcbc308a9094db15a7b6e75167c1f7f2
paul <pdmxdd@gmail.com>
2020-01-23 12:54:07 -0600

    cleaning up

--- tests/blueprints/bp_user_tests.py
+++ tests/blueprints/bp_user_tests.py
@@ -21,15 +21,15 @@ class BpUserTests(unittest.TestCase):
 
     def test_register_duplicate_user(self):
         path = "/user/register"
-        resp1 = requests.post(f"{self.server_url}{path}", json={"email": "email1@email.com", "password": "thepassword"})
+        resp1 = requests.post(f"{self.server_url}{path}", json={"email": "email@email.com", "password": "thepassword"})
         self.assertEqual(201, resp1.status_code)
-        resp2 = requests.post(f"{self.server_url}{path}", json={"email": "email1@email.com", "password": "thepassword"})
+        resp2 = requests.post(f"{self.server_url}{path}", json={"email": "email@email.com", "password": "thepassword"})
         self.assertNotEqual(201, resp2.status_code)
 
     def test_user_login(self):
         path = "/user/login"
-        requests.post(f"{self.server_url}/user/register", json={"email": "email2@email.com", "password": "thepassword"})
-        resp1 = requests.post(f"{self.server_url}{path}", json={"email": "email2@email.com", "password": "thepassword"})
+        requests.post(f"{self.server_url}/user/register", json={"email": "email@email.com", "password": "thepassword"})
+        resp1 = requests.post(f"{self.server_url}{path}", json={"email": "email@email.com", "password": "thepassword"})
         self.assertEqual(200, resp1.status_code)
-        resp2 = requests.post(f"{self.server_url}{path}", json={"email": "email2@email.comm", "password": "nopassword"})
+        resp2 = requests.post(f"{self.server_url}{path}", json={"email": "email@email.comm", "password": "nopassword"})
         self.assertEqual(400, resp2.status_code)
