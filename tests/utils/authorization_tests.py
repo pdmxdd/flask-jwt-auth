bfdc10af2102f8a9e4c5b906e72ae912ab5e64d7
paul <pdmxdd@gmail.com>
2020-01-24 15:02:37 -0600

    JWT groundwork

---
+++ tests/utils/authorization_tests.py
@@ -0,0 +1,46 @@
+import unittest
+import time
+import datetime
+from calendar import timegm
+
+from util.authorization import assign_token, verify_token
+
+
+class AuthorizationTests(unittest.TestCase):
+
+    def test_assign_token(self):
+        token = assign_token({"status": "test", "body": "hello"}, "252.144.144.12")
+        self.assertEqual(str, type(token))
+        # token should have exactly 2 '.' splitting the string into 3 sections
+        self.assertEqual(3, len(token.split('.')))
+
+    def test_verify_default_token_success(self):
+        token = assign_token({"status": "test", "body": "hello"}, "252.144.144.12")
+        result = verify_token(token, "252.144.144.12")
+        self.assertTrue(result["success"])
+        self.assertEqual("test", result["payload"]["status"])
+        self.assertEqual("hello", result["payload"]["body"])
+        self.assertTrue("exp" in result["payload"].keys())
+        self.assertTrue("iat" in result["payload"].keys())
+        # token is issued by default for 6 hours (6 * 60 * 60 -> 21600)
+        self.assertEqual(21600, result["payload"]["exp"] - result["payload"]["iat"])
+
+    def test_verify_token_bad_ip(self):
+        token = assign_token({"status": "test", "body": "hello"}, "252.144.144.12")
+        result = verify_token(token, "111.111.111.111")
+        self.assertFalse(result["success"])
+        self.assertEqual("JWT error. IP Addresses don't match! User needs to re-authenticate.", result["status"])
+
+    def test_verify_token_bad_token(self):
+        token = assign_token({"status": "test", "body": "hello"}, "252.144.144.12")
+        result = verify_token(f"{token}a", "252.144.144.12")
+        self.assertFalse(result["success"])
+        self.assertEqual("JWT error. User needs to re-authenticate.", result["status"])
+
+    def test_expired_token(self):
+        token = assign_token({"status": "test", "body": "hello"}, "252.144.144.12", lifetime_hours=(1/3600))
+        time.sleep(2.0)
+        result = verify_token(token, "252.144.144.12")
+        self.assertFalse(result["success"])
+        self.assertEqual("JWT expired. User must re-authenticate for a new token.", result["status"])
+
---
+++ util/authorization.py
@@ -0,0 +1,50 @@
+import os
+import datetime
+import binascii
+import jwt
+
+
+def get_jwt_secret():
+    # This method either gets or creates a new jwt secret
+    if "jwt_secret" not in os.environ.keys():
+        if os.path.isfile('jwt_secret.txt'):
+            with open('jwt_secret.txt', 'r') as f:
+                os.environ["jwt_secret"] = f.read()
+        else:
+            jwt_secret = binascii.hexlify(os.urandom(32)).decode()
+            # jwt_secret = Fernet.generate_key().decode()
+            with open('jwt_secret.txt', 'w') as f:
+                f.write(jwt_secret)
+            os.environ["jwt_secret"] = jwt_secret
+    return os.environ["jwt_secret"]
+
+
+def assign_token(payload, ip_address, lifetime_hours=6):
+    payload["ip_address"] = ip_address
+    payload["iat"] = datetime.datetime.utcnow()
+    payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(seconds=(lifetime_hours*60*60))
+    encoded_token = jwt.encode(
+        payload,
+        get_jwt_secret(),
+        algorithm='HS256'
+    )
+    return encoded_token.decode('utf-8')
+
+
+def verify_token(token_string, ip_address):
+    key = get_jwt_secret()
+    if token_string == "":
+        return {"success": False, "status": "JWT error. No JWT provided"}
+    try:
+        decoded_token = jwt.decode(str.encode(token_string), key, algorithms=['HS256'])
+        if decoded_token["ip_address"] != ip_address:
+            return {"success": False, "status": "JWT error. IP Addresses don't match! User needs to re-authenticate."}
+    except jwt.ExpiredSignatureError:
+        # need to return JSON error that token is expired
+        # return False
+        return {"success": False, "status": "JWT expired. User must re-authenticate for a new token."}
+    except jwt.exceptions.InvalidSignatureError:
+        return {"success": False, "status": "JWT error. User needs to re-authenticate."}
+    except jwt.exceptions.DecodeError:
+        return {"success": False, "status": "JWT error. User needs to re-authenticate."}
+    return {"success": True, "payload": jwt.decode(str.encode(token_string), key, algorithms=['HS256'])}
